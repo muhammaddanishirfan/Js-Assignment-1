1a8e7e849a0455b963405b274dd9119cdde3742c
muhammaddanishirfan <danishanwer2008@gmail.com>
2024-09-06 17:57:34 +0500

    Add files via upload

---
+++ Assignment 1/app.js
@@ -0,0 +1,152 @@
+// // var myname = "Danish";
+// // console.log(myname)
+
+// // var firstname ="Muhammad"
+
+// // var secondname ="Danish"
+
+// // var city ="karachi"
+
+// // var education ="Matric"
+
+// // var chap 1 = Q1
+
+// alert("You Are Wellcome")
+
+// // var chap 1 = Q2
+
+// alert("Error! Please Enter A Valid Password")
+
+// // var chap 1 = Q3
+
+// alert("Wellcome to Js....\nHappy Coding!" )
+
+// // var chap 1 = Q4
+
+// alert("Wellcome to JS land...")
+
+// alert("Happy Coding! \nPrevent the page from creating additional dialoges")
+
+// // var chap 1 = Q5
+
+// alert("Hello...! I Can run Js through my web browsers console")
+
+// // var chap 1 = Q6
+
+// alert("ASSALAMA U ALAIKUM")
+
+// // var chap 1 = Q7
+
+
+
+// // var chap 2 = Q1
+
+// var username
+
+// // var chap 2 = Q2
+
+// var myName = "Muhammad Danish"
+
+// // var chap 2 = Q3 a
+
+// var message;
+
+// // var chap 2 = Q3 b
+
+// message = "Hello World"
+
+// // var chap 2 = Q3 c
+
+// alert(message)
+
+// // var chap 2 = q4
+
+// alert(myName)
+
+// var age = "16"
+
+// alert(age)
+
+// var qualification = "Matric"
+
+// alert(qualification)
+
+// // var chap 2 = Q5
+
+// alert("Pizza \nPizz \nPiz \nPi \nP" )
+
+// // var chap 2 = Q6
+
+// var email = "danishanwer2008@gmail.com"
+
+// alert(email)
+
+// // var chap 2 = Q7
+
+// var smarterwaytolearnjs = "i am trying ..............js"
+
+// alert(smarterwaytolearnjs)
+
+// // var chap 2 = Q8
+
+
+
+
+// // var chap 2 = Q9
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(design)
+
+
+// // var chap 3 = Q1
+
+// var myage = "i am 16 years old"
+
+// alert(myage)
+
+// // var chap 3 = Q2
+
+// var visitthissite = "You Have Visited This Site 14 Times"
+
+// alert(visitthissite)
+
+// var chap 3 = Q3
+
+var birthyear = "01-08-2008"
+
+document.write(birthyear )
+
+// var chap 3 = Q4
+
+var visitorsname = " Ali "
+
+var Producttitle = " T-Shirt "
+
+var Quantity = 20
+
+document.write("\n", visitorsname +  " Ordered " +  Quantity + Producttitle + " on XYZ Clothings tore")
+
+// var chap 4 = Q1-Q4
+
+var number = "number" 
+var $ = '$' 
+var _ = "_"
+var a = "a"
+var _ = "_"
+
+//illegal variables
+
+//var 1 = 
+
+//var & = 
+
+//var @ = 
+
+//var - = 
+
+//var * = 
+
+document.write = ("Rule for namin JS Variables")
+
+document.write =("Variables name can only contain",number,$,"and",_,"for example :",$,"m",_,"1st variable \n variable must begin with a leter , " , $ , "or" , _ , ".for example:" , $ ,"name or name \n Variables name are case sensitive \n Variable name should not be JS Keywords")
